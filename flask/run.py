a065015678b32b4964791a5fd2bb49becbeb0961
kouichi-11” <itinose.ko.1@gmail.com>
2021-02-28 10:35:20 +0900

    jetracer

--- flask/run.py
+++ flask/run.py
@@ -2,10 +2,16 @@
 from flask import Flask,render_template,request
 from flask_cors import CORS
 
+#jetracer
+from jetracer.nvidia_racecar import NvidiaRacecar
+car = NvidiaRacecar()
+
 #Flaskオブジェクトの生成
 app = Flask(__name__, static_folder="../react/build/static", template_folder="../react/build")
 CORS(app)
 
+def Map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 @app.route("/")
 def index():
@@ -14,11 +20,23 @@ def index():
 @app.route("/speed/<int:speed>", methods=["POST"])
 def speed(speed):
     print(speed)
+    throttle = Map(speed, 0, 100, 0, 1) * -1
+    if(speed < 10):
+        car.throttle = 0
+    else:
+        car.throttle = throttle
+    print(throttle)
     return 'Speed: %d' % speed
 
 @app.route("/steering/<int:steering>", methods=["POST"])
 def steering(steering):
     print(steering)
+    steering_map = Map(steering, 0, 100, -0.8, 0.8) 
+    if(45 < steering < 55):
+        car.steering = 0
+    else:
+        car.steering = steering_map
+    print(steering_map)
     return 'Steering: %d' % steering
 
 if __name__ == "__main__":
